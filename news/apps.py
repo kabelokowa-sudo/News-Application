8124ecbef8796202baa4988882aca34d7f894ed4
Kabelo <kabelokowa@gmail.com>
2026-09-04 09:47:27 +0000

    docs: add docstring to NewsConfig

--- news/apps.py
+++ news/apps.py
@@ -2,6 +2,12 @@ from django.apps import AppConfig
 
 
 class NewsConfig(AppConfig):
+    """Django app configuration for the news app.
+
+    Registers the ``news.signals`` module on app startup so the
+    ``sync_user_role`` signal receiver is connected.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news'
 
